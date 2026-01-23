f5c2f89599cd5edc1035ed417280092d73537729
gamarnath7 <165361555+gamarnath7@users.noreply.github.com>
2026-01-23 22:24:33 +0530

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,23 @@
+class Dog:
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over!")
+my_dog = Dog('Willie', 6)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+my_dog.roll_over()
+your_dog=Dog('Lucy',3)
+print(f"\n Your dog' name is {your_dog.name}")
+print(f"Your dog id {your_dog.age} years old")
+your_dog.sit()
+your_dog.roll_over()
+    
+
